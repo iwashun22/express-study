f1203b5cc21c7a2b47b2b8405548b1f1ab949b25
Shun Iwashita <iwashunabc519@gmail.com>
2021-11-08 20:20:32 +0700

    udpate

--- second.js
+++ second.js
@@ -10,12 +10,29 @@ const logger = (req, res, next) => {
    const day = time.getDate();
    const stringify = year + '-' + month + '-' + day;
    console.log(method, url, stringify);
-   
+
    next(); // you need this callback to execute the third parameter of get()
 }
 
-app.get('/', logger, (req, res) => {
+// you can add middleware function by this
+
+// app.get('/', logger, (req, res) => {
+//    res.status(200).send('<h1>Home</h1>')
+// })
+
+
+
+// But you might noticed that if the function will be use everytime, its still a long code
+// You can simply use app.use() to extends all of the method to include the middleware
+
+app.use(logger);
+
+app.get('/', (req, res) => {
    res.status(200).send('<h1>Home</h1>')
+})
+
+app.get('/about', (req, res) => {
+   res.status(200).send('<h1>About</h1>');
 })
 
 app.listen(3000, () => {
---
+++ third.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { users } = require('./data');
+const app = express();
+
+const apiLogger = (req, res, next) => {
+   const { method, url } = req;
+   console.log(method, url);
+   next();
+}
+
+// this will includes all of the url that start with /api
+// /api => included
+// /api/home/users => included
+// /api/id/:id => included
+// /home/api => not included
+app.use('/api', apiLogger);
+
+app.get('/home', (req, res) => {
+   res.send('<h1>Home</h1>');
+})
+
+app.get('/home/api', (req, res) => {
+   res.send('<h1>API</h1>');
+})
+
+app.get('/api', (req, res) => {
+   const json = users.map(user => { 
+      const { id, name, email } = user;
+      return { id, name, email }
+   });
+   res.json(json);
+})
+
+app.get('/api/id/:id', (req, res) => {
+   const id = req.params.id;
+   const getUser = users.find(user => user.id === +id);
+   delete getUser.description;
+   if(getUser){
+      return res.json(getUser);
+   }
+   res.send('<h1>Could not find the user</h1>');
+})
+
+app.all('*', (req, res) => {
+   res.status(404).send('<h1>Could not find the page</h1>');
+})
+
+app.listen(3000, () => {
+   console.log('listening on port 3000...');
+})
