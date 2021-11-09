d0be55068cfba8220a2dfed696ac36ae8f850b9d
Shun Iwashita <iwashunabc519@gmail.com>
2021-11-09 20:49:48 +0700

    update

--- post-method.js
+++ post-method.js
@@ -3,6 +3,15 @@ const app = express();
 
 app.use(express.static('./post'));
 
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/login', (req, res) => {
+   const data = req.body;
+   console.log(data);
+   res.send('POST');
+})
+
 app.listen(8000, () => {
    console.log('listening on port 8000...');
 })
